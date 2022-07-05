e1cc2eda8d74714439856c0546839d5f92855b5f
Milton Rodrigues <rodriguesmilton21@gmail.com>
2022-07-05 23:25:17 +0530

    07/05/2022 23:20 Accepted 166 ms 24.2 MB python3

---
+++ 143. Reorder List.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head,head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+           
+        second = slow.next
+        prev = slow.next = None
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        
+        first,second = head, prev
+        # because second half is shorter than the first half
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2

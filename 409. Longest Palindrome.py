2962d3435f99888ce85e3583551c7c21cfad8f99
@Milton.Rodrigues <milton.rodrigues@refinitiv.com>
2022-08-23 22:47:45 +0530

    -

---
+++ 409. Longest Palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        HashMap = {}
+        result = 0
+        for c in s:
+            HashMap[c] = HashMap.get(c,0) + 1
+        for i in HashMap.keys():
+            result = result + (HashMap[i]//2) * 2
+            if result % 2 == 0 and HashMap[i] % 2 == 1:
+                result = result + 1
+        return result
+                
+                
+        
+            
+        
